79da30ebac68e8644af3fd89a9a22f6cb15013fa
MIKE FOSTER <HELLOMIKEFOSTER@GMAIL.COM>
2023-10-12 14:14:20 +0100

    Should have tested more! Add clear storage function

--- results.js
+++ results.js
@@ -28,6 +28,12 @@ function grabLocalStorage() {
   }
 }
 
+// Clear local storage when hitting the Start Again button
+function clearStorage() {
+  localStorage.clear();
+  history.back();
+}
+
 // Invoke it early so the code has data to work with
 grabLocalStorage();
 
